36c01d1691e329cbd9b026b70f679b1cb8457ac4
jonyalmeida <jony.figs@gmail.com>
2020-09-26 15:54:02 -0500

    Configure app.js + config/index.js + /config/database.js

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.use(routes);
 // Serve React Application
 // This should come after routes, but before 404 and error handling.
 if (process.env.NODE_ENV === "production") {
+  app.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
   app.use(express.static("client/build"));
   app.get(/\/(?!api)*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
@@ -42,11 +46,11 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-app.use(function(_req, _res, next) {
+app.use(function (_req, _res, next) {
   next(createError(404));
 });
 
-app.use(function(err, _req, res, _next) {
+app.use(function (err, _req, res, _next) {
   res.status(err.status || 500);
   if (err.status === 401) {
     res.set('WWW-Authenticate', 'Bearer');
