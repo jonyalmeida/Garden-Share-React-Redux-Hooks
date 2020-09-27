b36929d6fb2bad028adca8c9dea6eb328c8fc286
jonyalmeida <jony.figs@gmail.com>
2020-09-26 21:31:20 -0500

    add login redux action

---
+++ client/src/store/auth.js
@@ -0,0 +1,29 @@
+import Cookies from 'js-cookie';
+
+const SET_USER = 'auth/SET_USER';
+
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        user,
+    };
+};
+
+export const login = (email, password) => {
+    return async dispatch => {
+        const res = await fetch('/api/session', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        res.data = await res.json(); // { user: { ... }}
+        if (res.ok) {
+            dispatch(setUser(res.data));
+        }
+        return res;
+    };
+};
+
